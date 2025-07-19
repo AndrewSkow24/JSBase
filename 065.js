4cb6b2e6f6f614a4581727ba6bdb76dbb0f139db
Andrew Skow <anjeie24@mail.ru>
2025-07-20 00:43:35 +0300

    65of503

---
+++ 065.js
@@ -0,0 +1,85 @@
+// Поиск ошибок в коде
+// В следующих задачах некоторый программист написал код и,
+// возможно, допустил в нем ошибки.
+// Вы должны проверить, делает ли код то, что описано.
+// Если код работает некорректно, вы должны исправить ошибки.
+
+// №1⊗jsPmBsMst
+// Код должен находить сумму чисел:
+let num1 = 1;
+let num2 = 2;
+console.log("сумма: " + (+num1 + +num2));
+
+// №2⊗jsPmBsMst
+// Код должен находить сумму чисел:
+let a = 1;
+let b = 2;
+let c = 0;
+console.log(a + b + c);
+
+// №3⊗jsPmBsMst
+// Код должен находить сумму цифр числа:
+let num = "123";
+num = String(num);
+let sum = +num[0] + +num[1] + +num[2];
+console.log(sum);
+
+// №4⊗jsPmBsMst
+// Код должен вывести первую цифру числа:
+num = 123;
+num = String(num);
+console.log(num[0]);
+
+// №5⊗jsPmBsMst
+// Код должен был вывести в консоль число 1, но выводит 0:
+a = 0;
+console.log(++a);
+
+// №6⊗jsPmBsMst
+// Код должен вывести количество цифр в числе:
+num = 123;
+num = String(num);
+console.log(num.length);
+
+// №7⊗jsPmBsMst
+// Код должен найти количество секунд в сутках:
+a = 24 * 60 * 60;
+console.log(a);
+
+// №8⊗jsPmBsMst
+// Код должен вывести количество цифр в числе:
+num = 123;
+let str = String(num);
+console.log(str.length);
+
+// №9⊗jsPmBsMst
+// Код должен вывести последнюю цифру числа:
+num = 123;
+str = String(num);
+console.log(str[str.length - 1]);
+
+// №10⊗jsPmBsMst
+// Код должен вывести количество цифр в числе:
+num = 123;
+str = String(num);
+console.log(str.length);
+
+// №11⊗jsPmBsMst
+// Код должен вывести последнюю цифру числа:
+num = 123;
+str = String(num);
+console.log(str[str.length - 1]);
+
+// №12⊗jsPmBsMst
+// Код должен найти сумму чисел:
+a = "123";
+b = "456";
+let s = Number(a) + Number(b);
+console.log(s);
+
+// №13⊗jsPmBsMst
+// Код должен найти сумму чисел:
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
