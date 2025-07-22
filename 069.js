c8558c3252971c3e579a3bf645a2b0e40297f96d
Andrew Skow <anjeie24@mail.ru>
2025-07-22 22:20:18 +0300

    69of504

---
+++ 069.js
@@ -0,0 +1,32 @@
+// №1⊗jsPmArEG
+// Создайте массив с элементами 1, 2, 3.
+// Выведите на экран каждый из этих элементов.
+console.log("Задача 1");
+let mySimpleArr = [1, 2, 3];
+console.log(mySimpleArr[0]);
+console.log(mySimpleArr[1]);
+console.log(mySimpleArr[2]);
+
+// №2⊗jsPmArEG
+// Дан следующий массив:
+let arr = [1, 2, 3];
+// Выведите на экран каждый из его элементов.
+console.log("Задача 2");
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+// №3⊗jsPmArEG
+// Дан следующий массив:
+arr = [1, 2, 3];
+// Выведите на экран сумму элементов этого массива.
+console.log("Задача 2");
+console.log(arr[0] + arr[1] + arr[2]);
+
+// №4⊗jsPmArEG
+// Дан следующий массив:
+arr = ["a", "b", "c", "d"];
+// Выведите с помощью этого массива следующую строку:
+// 'a+b+c+d'
+console.log("Задача 4");
+console.log(`${arr[0]}+${arr[1]}+${arr[2]}+${arr[3]}`);
